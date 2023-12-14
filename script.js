b4e9ee96e7dfcff7b617a921f89909ce59e67933
Natalie <natalie.dflcn@gmail.com>
2023-12-13 19:31:38 -0500

    Implemented game logic for when user correctly guesses the number

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 'use strict';
+const number = Math.floor(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -6,5 +8,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
   }
 });
