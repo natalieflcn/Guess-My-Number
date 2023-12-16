8459ce07e098d028c12948d034b5d606e3feb542
Natalie <natalie.dflcn@gmail.com>
2023-12-15 20:32:56 -0500

    Refactored a querySelector method into a function

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let number = Math.floor(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
-//document.querySelector('.number').textContent = number;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -11,11 +13,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
   //When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
 
     //When the player wins
   } else if (guess === number) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -29,14 +31,14 @@ document.querySelector('.check').addEventListener('click', function () {
     //When the guess is wrong
   } else if (guess !== number) {
     guess < number
-      ? (document.querySelector('.message').textContent = '📉 Too low!')
-      : (document.querySelector('.message').textContent = '📈 Too high!');
+      ? displayMessage('📉 Too low!')
+      : displayMessage('📈 Too high!');
     score--;
     document.querySelector('.score').textContent = score;
 
     //When the player loses
   } else if (score === 1) {
-    document.querySelector('.message').textContent = '🤕 You lost!';
+    displayMessage('🤕 You lost!');
     score--;
     document.querySelector('.score').textContent = score;
   }
@@ -49,7 +51,7 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   document.querySelector('.score').textContent = score;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.number').textContent = '?';
 
   document.querySelector('.guess').value = false;
